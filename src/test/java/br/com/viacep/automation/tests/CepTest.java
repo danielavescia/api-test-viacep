ab9fa4265f4674bca01df43aa42039b10bea4b78
danielavescia <danivescia@gmail.com>
2026-06-05 12:34:16 -0300

    test: implement schema validation test for successful CEP response and remove obsolete config file

--- src/test/java/br/com/viacep/automation/tests/CepTest.java
+++ src/test/java/br/com/viacep/automation/tests/CepTest.java
@@ -3,6 +3,7 @@ package br.com.viacep.automation.tests;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import br.com.viacep.automation.assertions.CepAssert;
 import br.com.viacep.automation.builders.RequestBuilder;
 import br.com.viacep.automation.builders.ResponseBuilder;
@@ -48,5 +49,17 @@ public class CepTest {
                 .get()
             .then()
                 .spec(ResponseBuilder.getBadRequestResponseSpec());
+    }
+
+    @Test(description = "CT04- Validar schema da resposta para CEP válido", dataProvider = "validCeps",  dataProviderClass = CepDataProvider.class)
+    public void shouldValidateSuccessResponseSchema(CepScenario scenario){
+            given()
+                .spec(RequestBuilder.getDefaultRequestSpec())
+                .pathParam("cep", scenario.getInput())
+            .when()
+                .get()
+            .then()
+                .spec(ResponseBuilder.getSucessResponseSpec())
+                .body(matchesJsonSchemaInClasspath("schemas/sucess-response-schema.json"));
     }
 }
