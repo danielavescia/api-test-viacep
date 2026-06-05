a669c7a212a49ee4349aa946a9d8646c77bdf683
danielavescia <danivescia@gmail.com>
2026-06-05 10:07:36 -0300

    feat: add CepAssert class for asserting equality of Cep objects in tests

---
+++ src/test/java/br/com/viacep/automation/assertions/CepAssert.java
@@ -0,0 +1,28 @@
+package br.com.viacep.automation.assertions;
+
+import org.testng.asserts.SoftAssert;
+
+import br.com.viacep.automation.pojo.Cep;
+
+public class CepAssert {
+
+    public static void assertEquals(Cep actual, Cep expected){
+        
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertEquals(actual.getCep(), expected.getCep());
+        softAssert.assertEquals(actual.getLogradouro(), expected.getLogradouro());
+        softAssert.assertEquals(actual.getBairro(), expected.getBairro());
+        softAssert.assertEquals(actual.getLocalidade(), expected.getLocalidade());
+        softAssert.assertEquals(actual.getUf(), expected.getUf());
+        softAssert.assertEquals(actual.getEstado(), expected.getEstado());
+        softAssert.assertEquals(actual.getRegiao(), expected.getRegiao());
+        softAssert.assertEquals(actual.getDdd(), expected.getDdd());
+        
+        softAssert.assertAll();
+    }
+
+    public static void assertErrorEquals(){
+        
+    }
+}
