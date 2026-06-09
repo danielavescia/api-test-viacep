25397a66b226293a0ceb04293d4aee1cc02b4719
danielavescia <danivescia@gmail.com>
2026-06-09 15:33:36 -0300

    chore: remove scope test from restassured dependency and move folders to main

--- src/main/java/br/com/viacep/automation/builders/RequestBuilder.java
+++ src/main/java/br/com/viacep/automation/builders/RequestBuilder.java
@@ -5,7 +5,6 @@ import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
-
 public class RequestBuilder {
 
 private static final String VIACEP_BASE_URL = "https://viacep.com.br/ws";
